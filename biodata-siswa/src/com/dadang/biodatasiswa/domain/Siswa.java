3f95788fced681197222d5ba16eae5c314634cd9
dadang <dadangiswan@gmail.com>
2015-10-13 11:47:41 +0700

    add project biodata siswa menggunakan sqlite

---
+++ biodata-siswa/src/com/dadang/biodatasiswa/domain/Siswa.java
@@ -0,0 +1,37 @@
+package com.dadang.biodatasiswa.domain;
+
+/**
+ * Created by dadang on 14/09/15.
+ */
+
+public class Siswa {
+
+    private String id;
+    private String nama;
+    private String kelas;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+}
